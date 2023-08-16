2ba3321907aaf60cd22bd8f51930828c95cb273e
MvuleniNofemele <133951877+MvuleniNofemele@users.noreply.github.com>
2023-08-16 18:37:48 +0200

    Update Absa_Idirect.java

--- src/test/java/sapiens/policies/Idirect/Absa_Idirect.java
+++ src/test/java/sapiens/policies/Idirect/Absa_Idirect.java
@@ -15,7 +15,7 @@ public class Absa_Idirect extends WebDr {
         this.wdriver = wdriver;
         this.test = test;
     }
-
+/**Mvuleni commented on this code**/
     String sCampaignType = this.getValue("CampaignType");
     String sStuffIndicator = this.getValue("StuffIndicator");
     String sCategory = this.getValue("Category");
